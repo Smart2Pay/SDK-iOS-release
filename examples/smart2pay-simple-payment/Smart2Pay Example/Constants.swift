799a98478732f8fd3652917f8125b760659410f7
David Jupijn <david@risingstep.com>
2018-12-18 13:26:07 +0100

    Add dummy credit card data

--- examples/smart2pay-simple-payment/Smart2Pay Example/Constants.swift
+++ examples/smart2pay-simple-payment/Smart2Pay Example/Constants.swift
@@ -15,3 +15,33 @@ let apiURL = "https://s2pmerchantserver.azurewebsites.net"
 let apiToken = "Basic UzJwTWVyY2hhbnRTZXJ2ZXI6UzJwTWVyY2hhbnRTZXJ2ZXJQYXNzd29yZA=="
 let wechatAppId = "wxb91c84b6c4d2e07b"
 let urlScheme = "com.smart2pay.example"
+
+func dummyCreditCardData() -> [String: Any] {
+    var body: [String: Any]  = [:]
+    var cardAuthentication: [String: Any]  = [:]
+    var customer: [String: Any]  = [:]
+    var billingAddress: [String: Any]  = [:]
+    var card: [String: Any]  = [:]
+    
+    customer["FirstName"] = "John"
+    customer["LastName"] = "Doe"
+    customer["Email"] = "android@sdktest.com"
+    customer["SocialSecurityNumber"] = "00003456789"
+    
+    billingAddress["Country"] = "BR"
+    
+    card["HolderName"] = "John Doe"
+    card["Number"] = "4111111111111111"
+    card["ExpirationMonth"] = "02"
+    card["ExpirationYear"] = "2019"
+    card["SecurityCode"] = "321"
+    
+    cardAuthentication["Customer"] = customer
+    cardAuthentication["BillingAddress"] = billingAddress
+    cardAuthentication["Card"] = card
+    
+    body["CardAuthentication"] = cardAuthentication
+    
+    return body
+}
+
